c940cf1fcb407ab498e26fbb63784483a7467df6
Janhvi <Janhvi2013031@akgec.ac.in>
2023-07-29 20:36:10 +0530

    initial commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,7 +1,23 @@
 import * as api from '../api'
 
-const getPosts = () => {
-    const action = {type: 'FETCH_ALL', payload: []}
+export const getPosts = () => async (dispatch) => { 
+    try {
+        const {data} = await api.fetchPosts();
 
-    return action;
+        dispatch ({type: 'FETCH_ALL', payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+       const {data} = await api.createPost(post);
+       
+       dispatch({type:'CREATE', payload: data});
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://tank:glh0wgfxncNSG4SV@cluster0.h4oyg85.mongodb.net/?retryWrites=true&w=majority'
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://shark:y9dR2gUKVw2vsa3I@cluster0.h4oyg85.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
