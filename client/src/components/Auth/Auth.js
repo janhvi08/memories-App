55be27a9b4b556fb6bf8e9a7542d9f41d04a6914
Janhvi <janhvirajpoot038@gmail.com>
2023-08-03 16:19:51 +0530

    bday commit

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,22 +8,33 @@ import Icon from './Icon';
 import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined'
 import useStyles from './styles';
 import Input from './Input';
+import {signin, signup} from '../../actions/auth';
+
+const initialState = {firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if (isSignup){
+            dispatch(signup(formData, history));
+        }
+        else {
+            dispatch(signin(formData, history));
+        }
     };
 
     const handleChange = () => {
-
+        setFormData({ ...formData, [e.target.name]: e.target.value});
     }
 
     const switchMode = () => {
@@ -62,7 +73,7 @@ const Auth = () => {
                         isSignup && (
                             <>
                                 <Input name="firstname" label="First Name" handleChange={handleChange} autoFocus half />
-                                <Input name="firstname" label="First Name" handleChange={handleChange} half />
+                                <Input name="lastname" label="Last Name" handleChange={handleChange} half />
                             </>
                         )}
                     <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 dotenv.config();
@@ -14,11 +15,12 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://shark:y9dR2gUKVw2vsa3I@cluster0.h4oyg85.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://shark:y9dR2gUKVw2vsa3I@cluster0.h4oyg85.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
